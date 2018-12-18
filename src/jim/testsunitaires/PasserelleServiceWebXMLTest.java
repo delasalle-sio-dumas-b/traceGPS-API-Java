0722cadc22f4d7c3a8381b671c010047dc18fd65
BenjaDindou <b.dumas423@laposte.net>
2018-12-18 14:38:33 +0100

    Voici les communs

--- src/jim/testsunitaires/PasserelleServiceWebXMLTest.java
+++ src/jim/testsunitaires/PasserelleServiceWebXMLTest.java
@@ -19,17 +19,57 @@ public class PasserelleServiceWebXMLTest {
 	
 	@Test
 	public void testConnecter() {
-		fail("Not yet implemented");
+		String msg = PasserelleServicesWebXML.connecter("admin", "adminnnnnnnn");
+		assertEquals("Erreur : authentification incorrecte.", msg);
+		
+		msg = PasserelleServicesWebXML.connecter("admin", Outils.sha1("mdpadmin"));
+		assertEquals("Administrateur authentifié.", msg);
+		
+		msg = PasserelleServicesWebXML.connecter("europa", Outils.sha1("mdputilisateur"));
+		assertEquals("Utilisateur authentifié.", msg);	
 	}
 		
 	@Test
 	public void testCreerUnUtilisateur() {
-		fail("Not yet implemented");
+		String msg = PasserelleServicesWebXML.creerUnUtilisateur("jim", "delasalle.sio.eleves@gmail.com", "1122334455");
+		assertEquals("Erreur : pseudo trop court (8 car minimum) ou déjà existant.", msg);
+		
+		msg = PasserelleServicesWebXML.creerUnUtilisateur("turlututu", "delasalle.sio.elevesgmail.com", "1122334455");
+		assertEquals("Erreur : adresse mail incorrecte ou déjà existante.", msg);
+
+		msg = PasserelleServicesWebXML.creerUnUtilisateur("turlututu", "delasalle.sio.eleves@gmailcom", "1122334455");
+		assertEquals("Erreur : adresse mail incorrecte ou déjà existante.", msg);
+		
+		msg = PasserelleServicesWebXML.creerUnUtilisateur("turlututu", "delasalle.sio.eleves@gmail.com", "1122334455");
+		assertEquals("Erreur : adresse mail incorrecte ou déjà existante.", msg);
+		
+		msg = PasserelleServicesWebXML.creerUnUtilisateur("turlututu", "delasallesioeleves@gmail.com", "1122334455");
+		assertEquals("Enregistrement effectué ; vous allez recevoir un courriel avec votre mot de passe.", msg);
+		
+		msg = PasserelleServicesWebXML.creerUnUtilisateur("turlututu", "de.la.salle.sio.eleves@gmail.com", "1122334455");
+		assertEquals("Erreur : pseudo trop court (8 car minimum) ou déjà existant.", msg);	
 	}
 	
 	@Test
 	public void testSupprimerUnUtilisateur() {
-		fail("Not yet implemented");	
+		String msg;
+		msg = PasserelleServicesWebXML.supprimerUnUtilisateur("europa", Outils.sha1("mdputilisateurrrrrr"), "toto");
+		assertEquals("Erreur : authentification incorrecte.", msg);
+		
+		msg = PasserelleServicesWebXML.supprimerUnUtilisateur("europa", Outils.sha1("mdputilisateur"), "toto");
+		assertEquals("Erreur : authentification incorrecte.", msg);
+		
+		msg = PasserelleServicesWebXML.supprimerUnUtilisateur("admin", Outils.sha1("mdpadminnnnn"), "toto");
+		assertEquals("Erreur : authentification incorrecte.", msg);
+		
+		msg = PasserelleServicesWebXML.supprimerUnUtilisateur("admin", Outils.sha1("mdpadmin"), "toto");
+		assertEquals("Erreur : pseudo utilisateur inexistant.", msg);
+		
+		msg = PasserelleServicesWebXML.supprimerUnUtilisateur("admin", Outils.sha1("mdpadmin"), "neon");
+		assertEquals("Erreur : suppression impossible ; cet utilisateur possède encore des traces.", msg);	
+		
+		msg = PasserelleServicesWebXML.supprimerUnUtilisateur("admin", Outils.sha1("mdpadmin"), "turlututu");
+		assertEquals("Suppression effectuée ; un courriel va être envoyé à l'utilisateur.", msg);	
 	}
 	
 	@Test
@@ -69,7 +109,16 @@ public class PasserelleServiceWebXMLTest {
 	
 	@Test
 	public void testSupprimerUnUnParcours() {
-		fail("Not yet implemented");
-	}
-	
+		String msg = PasserelleServicesWebXML.supprimerUnParcours("europa", Outils.sha1("mdputilisateurrrrrr"), 10);
+		assertEquals("Erreur : authentification incorrecte.", msg);
+		
+		msg = PasserelleServicesWebXML.supprimerUnParcours("europa", Outils.sha1("mdputilisateur"), 100);
+		assertEquals("Erreur : parcours inexistant.", msg);
+		
+		msg = PasserelleServicesWebXML.supprimerUnParcours("europa", Outils.sha1("mdputilisateur"), 22);
+		assertEquals("Erreur : vous n'êtes pas le propriétaire de ce parcours.", msg);	
+		
+		msg = PasserelleServicesWebXML.supprimerUnParcours("europa", Outils.sha1("mdputilisateur"), 30);
+		assertEquals("Parcours supprimé.", msg);	
+	}	
 } // fin du test
